89b4d7dd31bf8fa18af1266134583894e44fc098
AhnYeonghoo <dksdudgn00@naver.com>
2022-09-14 09:24:21 +0900

    First Commit

---
+++ app/src/main/java/com/example/practice/main.kt
@@ -0,0 +1,28 @@
+package com.example.practice
+
+import java.lang.Integer.sum
+
+// 주석입니다.
+
+class Person(firstName: String, lastName: String) {
+    init {
+        println("${firstName} + ${lastName}")
+    }
+}
+fun main() {
+    println("hello World!!")
+    println("Line2")
+    println("Line3")
+    println("Line4")
+
+    val box1 = "Test Box1"
+    val asdf = "Text Box2"
+    println(box1)
+    println(asdf)
+   val number: Int? = null
+   val sumNumberStr = number?.let {
+       "${sum(10, it)}"
+   }.orEmpty()
+    println(sumNumberStr)
+
+}
